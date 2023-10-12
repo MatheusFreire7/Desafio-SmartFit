5a117994b9e2893ab002339026e5216fdaed2ba2
MatheusFreire7 <cc21251@g.unicamp.br>
2023-10-12 09:32:08 -0300

    Update Forms

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -7,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsComponent {
     results = [];
+    formGroup!: FormGroup;
 
-    constructor() {}
+    constructor(private formBuilder: FormBuilder) {}
 
     ngOnInit(): void {
+      this.formGroup = this.formBuilder.group({
+        hour: '',
+        showClosed: true
+      })
+    }
 
+    onSubmit(): void {
+      let { showClosed, hour } = this.formGroup.value
+    }
+  
+    onClean(): void {
+      this.formGroup.reset();
     }
 }
