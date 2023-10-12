8855b2bc4df46f32df3bccbdf073caf3f4b1a4ae
MatheusFreire7 <cc21251@g.unicamp.br>
2023-10-12 13:37:13 -0300

    Add Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { CardsListComponent } from './components/cards-list/cards-list.component';
 import { CardComponent } from './components/card/card.component';
 import { LegendComponent } from './components/legend/legend.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { LegendComponent } from './components/legend/legend.component';
     FormsComponent,
     CardsListComponent,
     CardComponent,
-    LegendComponent
+    LegendComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
